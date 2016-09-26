b0f1a089ce8d9e83dd3ac3398c02a04acb776429
Ricardo Mendes <eng.rmendes@gmail.com>
2016-09-26 20:10:12 -0300

    Correcting Migrate Class Name

--- database/migrations/2016_09_01_175947_create_product_images_table.php
+++ database/migrations/2016_09_01_175947_create_product_images_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Createadmin.product.image.indexTable extends Migration
+class CreateProductImagesTable extends Migration
 {
     /**
      * Run the migrations.
