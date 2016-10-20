40ee5d51ab99ea7475ee8a92e7e5e351e60943cb
Ricardo Mendes <eng.rmendes@gmail.com>
2016-10-19 23:39:46 -0200

    Managing assets with Elixir

--- resources/views/store/store.blade.php
+++ resources/views/store/store.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,13 +7,7 @@
     <meta name="author" content="">
     <title>Home | E-Shop</title>
 
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/prettyPhoto.css')}}" rel="stylesheet">
-    <link href="{{asset('css/animate.css')}}" rel="stylesheet">
-    <link href="{{asset('css/main.css')}}" rel="stylesheet">
-    <link href="{{asset('css/responsive.css')}}" rel="stylesheet">
-
+    <link href="{{elixir('css/all.css')}}" rel="stylesheet">
 
 </head><!--/head-->
 
@@ -144,11 +137,7 @@
 
 </footer><!--/Footer-->
 
-<script src="{{asset('js/jquery.js')}}"></script>
-<script src="{{asset('js/bootstrap.min.js')}}"></script>
-<script src="{{asset('js/jquery.scrollUp.min.js')}}"></script>
-<script src="{{asset('js/jquery.prettyPhoto.js')}}"></script>
-<script src="{{asset('js/main.js')}}"></script>
+<script src="{{elixir('js/all.js')}}"></script>
 
 </body>
 </html>
